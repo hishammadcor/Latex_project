41f2b8aacca0d09907ff0417a7c12caf2217097a
hishammadcor <hisham3madcor@gmail.com>
2024-03-21 12:23:32 +0100

    AQdd the style and tried some regex

--- test_latex_string.py
+++ test_latex_string.py
@@ -1,5 +1,42 @@
 import re
-import csv
 import pandas as pd
-import string 
+
+
+# style92 = r"""
+#                 \begin{tabular}{l s c c s s s}
+#                     \textit{ }   &    \textit{ }  &   \multicolumn{2}{c}{\textit{ }}  &
+#                     \multicolumn{3}{c}{\cellcolor{green40}\textit{ }} \\\hline
+#
+#                     &   &   &   &   &   &  \\ \hline
+#                     &   &   &   &   &   &   \\ \hline
+#                     &   &   &   &   &   &
+#
+#                 \end{tabular}
+#            """
+
+style92 = r"""
+\begin{tabular}{l s c c s s s}
+    \textit{ }   &    \textit{ }  &   \multicolumn{2}{c}{\textit{ }}  &
+    \multicolumn{3}{c}{\cellcolor{green40}\textit{ }} \\\hline
+    
+    %s
+    
+\end{tabular}
+"""
+path = 'style92.csv'
+
+df = pd.read_csv(path, delimiter=";")
+
+
+table_content = ""
+
+
+for index, row in df.iterrows():
+
+    table_content += ' & '.join(map(str, row)) + ' \\\\\n'
+
+styled_table = re.sub(r'(?<=\\\\hline\n\n)(.*?)(?=\\\\hline)', table_content.strip(), style92, flags=re.DOTALL)
+
+print(styled_table)
+
 
